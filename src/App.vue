f657c8ca41b55b2f4505179cad7da0de4cd1d25d
NicolaPlacentino <nyko0921@gmail.com>
2023-01-26 13:08:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from 'axios';
+import { store } from './store';
+import AppHeader from './components/AppHeader.vue';
+import BaseCardSection from './components/BaseCardSection.vue';
+export default {
+  name: 'App',
+  components: { AppHeader, BaseCardSection, BaseCard }
+}
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <BaseCardSection></BaseCardSection>
+  <BaseCardSection></BaseCardSection>
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
---
+++ src/components/BaseCardSection.vue
@@ -0,0 +1,15 @@
+<script>
+import BaseCard from './BaseCard.vue';
+export default {
+    name: 'BaseCardSection',
+    components: { BaseCard }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
